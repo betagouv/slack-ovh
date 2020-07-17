0d5c1470bd109b1838f9e246c9f4e91d6b1df3a4
Florian Delezenne <florian.delezenne@beta.gouv.fr>
2020-07-17 11:30:31 +0200

    Ajout descriptions manquantes

--- config.js
+++ config.js
@@ -9,6 +9,18 @@ const config = {
   slackChannel: "incubateur-secretaria",
   lists: [
     {
+      id: "acceslibre",
+      description:
+        "Equipe AccesLibre",
+      realMailingList: true
+    },
+    {
+      id: "aidantsconnect",
+      description:
+        "Equipe Aidants Connect",
+      realMailingList: true
+    },
+    {
       id: "aide-territoires",
       description:
         "Equipe Aides Territoires",
@@ -33,9 +45,26 @@ const config = {
       realMailingList: true
     },
     {
+      id: "biodiversite",
+      description:
+        "Equipe Biodiversite",
+      realMailingList: true
+    },
+    {
+      id: "camino",
+      description:
+        "Equipe Camino",
+      realMailingList: true
+    },
+    {
       id: "cartobio",
       description:
         "Equipe CartoBio",
+      realMailingList: true
+    },
+    {
+      id: "coachs",
+      description: "Partage d'informations des coachs",
       realMailingList: true
     },
     {
@@ -149,14 +178,31 @@ const config = {
       realMailingList: true
     },
     {
+      id: "fast",
+      description: "Demandes entrantes des appels à candidature FAST",
+      realMailingList: true
+    },
+    {
       id: "incubateur",
       description:
         "Tous les membres ayant un contrat en cours sur beta.gouv.fr/communaute",
       realMailingList: true
     },
     {
+      id: "investigations",
+      description:
+        "Contacts entrants de l'équipe investigations",
+      realMailingList: true
+    },
+    {
       id: "lapins",
       description: "Liste pour les membres de la Startup d'État Lapins",
+      realMailingList: true
+    },
+    {
+      id: "mobilic",
+      description:
+        "Equipe Mobilic",
       realMailingList: true
     },
     {
@@ -171,8 +217,35 @@ const config = {
       realMailingList: true
     },
     {
+      id: "passculture",
+      description: "Équipe du pass culture",
+      realMailingList: true
+    },
+    {
       id: "passculture-bizdev@beta.gouv.fr",
       description: "Les bizdevs du pass culture"
+    },
+    {
+      id: "passculture-dev@beta.gouv.fr",
+      description: "Les devs du pass culture"
+    },
+    {
+      id: "peps",
+      description:
+        "Equipe PEPS",
+      realMailingList: true
+    },
+    {
+      id: "plante-et-moi",
+      description:
+        "Equipe Plante et Moi",
+      realMailingList: true
+    },
+    {
+      id: "poubelles-battle",
+      description:
+        "Equipe Pouvelles Battle",
+      realMailingList: true
     },
     {
       id: "preincubation",
@@ -182,6 +255,29 @@ const config = {
     {
       id: "recrutement",
       description: "Tous les recrutements de beta.gouv.fr",
+      realMailingList: true
+    },
+    {
+      id: "recrutement-mineco",
+      description: "Les recrutements des MEF",
+      realMailingList: true
+    },
+    {
+      id: "secretariat",
+      description:
+        "Equipe PEPS",
+      realMailingList: true
+    },
+    {
+      id: "snu",
+      description:
+        "Equipe SNU",
+      realMailingList: true
+    },
+    {
+      id: "solidarite-fde",
+      description:
+        "Equipe Solidarite FDE",
       realMailingList: true
     }
   ]
