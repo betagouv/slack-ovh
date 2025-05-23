62cd2b17c6f105a5bf5e0db9b53e90eccc9939bb
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2025-05-23 12:33:25 +0200

    fix: doc (#97)
    * fix: doc
    
    * Update mails.js

--- routes/mails.js
+++ routes/mails.js
@@ -21,12 +21,13 @@ function extractEmail(email) {
 }
 
 const helpMessage = `Commandes disponibles:
-  \t- \`/emails list\`\t\tensemble des listes de diffusions existantes
-  \t- \`/emails list id_de_la_liste\`\t\tpersonnes inscrites dans la liste email_de_la_liste@domain.com
-  \t- \`/emails join id_de_la_liste email_a_ajouter@domain.com\`\tinscrire email_a_ajouter@domain.com à la liste email_de_la_liste@domain.com
-  \t- \`/emails leave id_de_la_liste email_a_ajouter@domain.com\`\tenlever email_a_ajouter@domain.com de la liste email_de_la_liste@domain.com
+  - \`/emails list\`: ensemble des listes de diffusions existantes
+  - \`/emails list id_de_la_liste\`: personnes inscrites dans la liste email_de_la_liste@domain.com
+  - \`/emails join id_de_la_liste email_a_ajouter@domain.com\`: inscrire email_a_ajouter@domain.com à la liste email_de_la_liste@domain.com
+  - \`/emails leave id_de_la_liste email_a_ajouter@domain.com\`: enlever email_a_ajouter@domain.com de la liste email_de_la_liste@domain.com
+  - \`/emails create id_de_la_liste admin@domain.com\`: créer la liste id_de_la_liste@beta.gouv.fr avec admin@domain.com comme propriétaire
 
-  Pour lire comment ajouter une nouvelle liste, c'est ici 👉 https://doc.incubateur.net/communaute/outils/liste-de-diffusion-et-adresses-de-contact#la-commande-slack-emails`;
+  Voir [la doc complète](https://doc.incubateur.net/communaute/les-outils-de-la-communaute/emails/liste-de-diffusion-et-adresses-de-contact#comment-creer-une-nouvelle-liste-de-diffusion-pour-sa-startup).`;
 
 const redirections = config.lists.reduce((acc, current) => {
   if (!current.realMailingList) {
